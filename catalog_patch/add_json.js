5b45acadbe54cf6add1291119dbbc5070d8a629f
Michael <mibamur@gmail.com>
2015-10-08 15:52:14 -0400

    Update add_json.js

--- catalog_patch/add_json.js
+++ catalog_patch/add_json.js
@@ -2,10 +2,6 @@ var fs = require('fs');
 var obj;
 var obj2 = JSON.parse(fs.readFileSync('catalog.json', 'utf8'));
 
-
-
-
-
 obj = JSON.parse(fs.readFileSync('community-polymer-elements.json', 'utf8'));
 obj2.packages.unshift(obj);
 obj = JSON.parse(fs.readFileSync('polymer-third-party.json', 'utf8'));
@@ -18,4 +14,3 @@ obj2.packages.unshift(obj);
 fs.writeFile('catalog.json', JSON.stringify(obj2, null, 4), 'utf8');
 // console.log('add first place - done');
 console.log(obj2);
-
