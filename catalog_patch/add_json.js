17ec5fd308d7b840cf6fff48187d39397014b0a0
Your Name <you@example.com>
2015-10-09 00:42:44 -0400

    add_json.js

--- catalog_patch/add_json.js
+++ catalog_patch/add_json.js
@@ -1,11 +1,21 @@
 var fs = require('fs');
-var obj = JSON.parse(fs.readFileSync('add.json', 'utf8'));
+var obj;
 var obj2 = JSON.parse(fs.readFileSync('catalog.json', 'utf8'));
 
-//obj2.packages.push(obj);
+
+
+
+
+obj = JSON.parse(fs.readFileSync('community-polymer-elements.json', 'utf8'));
+obj2.packages.unshift(obj);
+obj = JSON.parse(fs.readFileSync('polymer-third-party.json', 'utf8'));
+obj2.packages.unshift(obj);
+obj = JSON.parse(fs.readFileSync('marcus7777_utilities.json', 'utf8'));
+obj2.packages.unshift(obj);
+obj = JSON.parse(fs.readFileSync('marcus7777_development.json', 'utf8'));
 obj2.packages.unshift(obj);
 
 fs.writeFile('catalog.json', JSON.stringify(obj2, null, 4), 'utf8');
-console.log('add first place - done');
+// console.log('add first place - done');
 console.log(obj2);
 
